df803f6ff8d8c34c660669d0fe8eb58fac71cf2f
misun <eating8@naver.com>
2021-11-23 10:21:41 +0900

    API 호출

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=dd0315aa8755bd29d2fae374027a0297&itemPerPage=20")
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }, []);
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      {loading ? <h1>Loading...</h1> : null}
     </div>
   );
 }
