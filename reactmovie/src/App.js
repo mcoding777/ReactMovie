5dd001cb7560c23e866967dcf5024a3cbb8968d6
misun <mcoding777@naver.com>
2021-12-20 18:18:53 +0900

    최신영화 목록 API 호출

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -2,11 +2,19 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=dd0315aa8755bd29d2fae374027a0297&itemPerPage=20");
+    const json = await response.json();
+    console.log(json);
+    // setMovies(json.movieListResult.movieList);
+    // setLoading(false);
+  }
   useEffect(() => {
-    fetch("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=dd0315aa8755bd29d2fae374027a0297&itemPerPage=20")
-      .then(response => response.json())
-      .then(json => console.log(json))
+    getMovies()
   }, []);
+
   return (
     <div>
       {loading ? <h1>Loading...</h1> : null}
