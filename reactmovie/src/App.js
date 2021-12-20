e271f9ade41039a421a25a81fd7814c957de6393
misun <mcoding777@naver.com>
2021-12-21 03:31:00 +0900

    일별 박스오피스에서 장르 부분만 API 호출하는 과정 구현중

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -1,34 +1,59 @@
 import { useState, useEffect } from "react";
+import { getTodayDate } from './getTodayDate';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
+  const [boxoffice, setBoxOffice] = useState([]);
 
+  // 일별 박스오피스 API 추출
+  const apikey = "dd0315aa8755bd29d2fae374027a0297";
+  const targetDt = getTodayDate();
   const getMovies = async () => {
-    const response = await fetch("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=dd0315aa8755bd29d2fae374027a0297&itemPerPage=20");
-    const json = await response.json();
-    setMovies(json.movieListResult.movieList);
+    const movies = await fetch(`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${apikey}&targetDt=20211220`);
+    const movies_json = await movies.json();
+    setBoxOffice(movies_json.boxOfficeResult.dailyBoxOfficeList);
+    getGenres();
+  }
+  const getGenres = async () => {
+    for (let i=0; i<boxoffice.length; i++) {
+      const movieCd = boxoffice[i].movieCd;
+      const getGenre = await fetch(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${apikey}&movieCd=${movieCd}`)
+      const getGenre_json = await getGenre.json();
+      const genres = await getGenre_json.movieInfoResult.movieInfo.genres;
+      setBoxOffice(current => {
+        const newCurrent = [...current];
+        newCurrent[i]['genreNm'] = genres;
+        return newCurrent;
+      })
+    }
     setLoading(false);
   }
+
   useEffect(() => {
     getMovies()
   }, []);
 
-  console.log(movies);
+  console.log('boxoffice는',boxoffice);
 
   return (
     <div>
       {loading ? <h1>Loading...</h1> : 
-        <ul>
-          {movies.map((item) => {
+          boxoffice.map((item) => {
             return (
-                <li key={item.movieCd}>
-                  {item.movieNm}
-                </li>
+              <div key={item.movieCd}>
+                <h3>{item.movieNm}</h3>
+                <p>개봉일 : {item.openDt}</p>
+                <p>장르 : {item.genreNm.map((g, index) => {
+                  return (
+                    <span key={index}>
+                      {g}, 
+                    </span>
+                  )
+                })}</p>
+                <p>누적 관객수 : {Number(item.audiAcc).toLocaleString('ko-KR')}명</p>
+              </div>
             )
           })}
-        </ul>
-        }
     </div>
   );
 }
