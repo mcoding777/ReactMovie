eae663372438145cf9e4640c3b1f5747084e969e
misun <mcoding777@naver.com>
2021-12-20 21:44:29 +0900

    map 함수로 최신영화 20편 목록 아웃풋

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -7,17 +7,28 @@ function App() {
   const getMovies = async () => {
     const response = await fetch("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=dd0315aa8755bd29d2fae374027a0297&itemPerPage=20");
     const json = await response.json();
-    console.log(json);
-    // setMovies(json.movieListResult.movieList);
-    // setLoading(false);
+    setMovies(json.movieListResult.movieList);
+    setLoading(false);
   }
   useEffect(() => {
     getMovies()
   }, []);
 
+  console.log(movies);
+
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : null}
+      {loading ? <h1>Loading...</h1> : 
+        <ul>
+          {movies.map((item) => {
+            return (
+                <li key={item.movieCd}>
+                  {item.movieNm}
+                </li>
+            )
+          })}
+        </ul>
+        }
     </div>
   );
 }
